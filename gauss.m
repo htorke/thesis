83823f06a3975f0730db52949c9d3dbe8445d3f8
Harsha <torke045@umn.edu>
2020-09-23 17:48:14 -0500

    Added to gauss

--- gauss.m
+++ gauss.m
@@ -3,7 +3,10 @@ function [aperture] = gauss(X,Y,radius,center_x=0,center_y=0)
 %Optimum Strehl Ratio = 0.815
 
 %I(w0)/I(0) = 1/e^2
-w0 = 0.89*radius
+w0 = 0.89*radius;
+gauss = exp(-0.5*((x-u)/sigma).^2);%/(sigma*sqrt(2*pi));
+
+
 normpdf(x);
 
 aperture
